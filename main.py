6db61519a5da1c10b7e537153e40173ffe480932
Olga Budytska <olgabudickaja@gmail.com>
2025-02-04 13:43:54 +0200

    create interface

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from PIL import Image, ImageSequence
+import random
+
+from texts import text_rules_lines, text_welcome_lines, text_balls_lines, questions
 
 pygame.init()
 
@@ -14,18 +17,19 @@ icon = pygame.image.load("images/chto-chhochet-stat-mllionerom.png")
 bg_img = pygame.image.load("images/chto-chhochet-stat-mllionerom-bg.png")
 logo_img = pygame.image.load("images/chto-chhochet-stat-mllionerom.png")
 
+# loading rules img
+rule_1 = pygame.image.load("images/rule-1.png")
+rule_2 = pygame.image.load("images/rule-2.png")
+rule_3 = pygame.image.load("images/rule-3.png")
+rules_img = [rule_1, rule_2, rule_3]
+
+
 # loading fonts
-label_font = pygame.font.Font("fonts/Montserrat/Montserrat-SemiBold.ttf", 36)
-text_welcome_lines = [
-    "Вітаємо у грі:",
-    "Хто хоче стати мільйонером?",
-    'Для початку гри натисніть',
-    'ENTER'
-]
+label_font_36 = pygame.font.Font("fonts/Montserrat/Montserrat-SemiBold.ttf", 36)
+label_font_18 = pygame.font.Font("fonts/Montserrat/Montserrat-SemiBold.ttf", 18)
 
 text_color = (255, 255, 255)
-start_y = 200
-line_spacing = 50
+highlight_color = (245, 233, 66)
 fade_speed = 5
 
 # loading gif
@@ -59,39 +63,100 @@ pygame.mixer.music.set_volume(0.1)
 current_music = None
 
 gameplay = False
+rules = False
 run = True
+
+def render_text_lines(texts, start_x: int, start_y: int, label_font, line_spacing:int=50, center: bool = False):
+    """ Функція для рендерингу тексту з підсвіченими клавішами.
+        Якщо center=True, текст центрується по ширині екрану. """
+    for i, line_parts in enumerate(texts):
+        y = start_y + i * line_spacing
+
+        # Обчислюємо загальну ширину рядка для центрування
+        total_width = sum(label_font.render(part, True, text_color).get_width() for part in line_parts)
+
+        # Визначаємо початкову позицію x
+        x = (SCREEN_WIDTH - total_width) // 2 if center else start_x
+
+        for part in line_parts:
+            # Визначаємо колір: якщо це клавіша — підсвічуємо
+            color = highlight_color if part in ["a", "b", "c", "d", "n", "1", "2", "3", "ENTER", "q"] else text_color
+            text_surface = label_font.render(part, True, color)
+            screen.blit(text_surface, (x, y))
+            x += text_surface.get_width()
+
+def play_music(new_music):
+    """Функція для відтворення музики, перевіряє чи вже відтворюється."""
+    global current_music  # Використовуємо глобальну змінну
+    if current_music != new_music:
+        if current_music:
+            current_music.stop()
+        new_music.play(-1)
+        current_music = new_music
+
+# loading question
+used_questions = []
+def random_question(obj):
+    if not obj:
+        return None
+
+    while True:
+        random_idx = random.randint(0, len(obj) - 1)
+        if random_idx not in used_questions:
+            used_questions.append(random_idx)
+            return obj[random_idx]
+
+question = random_question(questions)
+print(question)
+question_label = label_font_18.render(question['question'], True, text_color)
+answer_label = None
+res_answer = False
 
 while run:
     screen.fill((7, 8, 12))
-    screen.blit(gif_frames[frame_index], (0, 0))
-    frame_counter += 1
 
     if frame_counter >= frame_delay:
         frame_index = (frame_index + 1) % len(gif_frames)
         frame_counter = 0
 
-    if not gameplay:
-        for i, line in enumerate(text_welcome_lines):
-            text_surface = label_font.render(line, True, text_color)
-            text_width = text_surface.get_width()
-            x = (SCREEN_WIDTH - text_width) // 2
-            screen.blit(text_surface, (x, start_y + i * line_spacing))
-
-        if current_music != welcome_music:
-            if current_music:
-                current_music.stop()
-            welcome_music.play(-1)
-            current_music = welcome_music
-    else:
+    if gameplay:
         screen.blit(bg_img, (0, 0))
-        screen.blit(logo_img, (250, 50))
+        screen.blit(logo_img, (250, 60))
+        current_x = 50
 
-        if current_music != gameplay_music:
-            if current_music:
-                current_music.stop()
-            gameplay_music.play(-1)
-            current_music = gameplay_music
+        for img in rules_img:
+            screen.blit(img, (current_x, 15))
+            current_x += 105
 
+        render_text_lines(text_balls_lines, 650, 20, label_font_18, 20)
+
+        screen.blit(question_label, (70, 405))
+
+        for answer in question['answers']:  # Перебираємо кожну відповідь
+            answer_text = answer['text']  # Отримуємо текст конкретної відповіді
+            answer_label = label_font_18.render(answer_text, True, text_color)
+            var_answer = answer_text.split(".")[0]  # Беремо першу частину (A, B, C, D)
+
+            match var_answer:
+                case "A":
+                    screen.blit(answer_label, (78, 471))
+                case "B":
+                    screen.blit(answer_label, (78, 519))
+                case "C":
+                    screen.blit(answer_label, (436, 471))
+                case "D":
+                    screen.blit(answer_label, (436, 519))
+
+        play_music(gameplay_music)
+    elif rules:
+        render_text_lines(text_rules_lines, 50, 50, label_font_18)
+    else:
+        screen.blit(gif_frames[frame_index], (0, 0))
+        frame_counter += 1
+
+        render_text_lines(text_welcome_lines, 50, 200, label_font_36, 50, center=True)
+
+        play_music(welcome_music)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -100,6 +165,10 @@ while run:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
                 gameplay = True
+            elif event.key == pygame.K_q:
+                rules = True
+
+
 
     pygame.display.update()
 
---
+++ texts.py
@@ -0,0 +1,158 @@
+text_rules_lines = [
+    ["Для відповіді на питання натисніть відповідну клавішу: ", "a", ", ", "b", ", ", "c", " або ", "d"],
+    ["За кожну правильну відповідь нараховуються бали"],
+    ["Для переходу на наступне питання натисніть клавішу ", "n"],
+    [""],
+    ["Можна скористатися однією з підказок."],
+    ["Всі підказки надаються один раз."],
+    ["1. Підказка 50/50: натисніть клавішу ", "1"],
+    ["2. Допомога залу: натисніть клавішу ", "2"],
+    ["3. Дзвінок другу: натисніть клавішу ", "3"],
+    ["Для початку гри натисніть ", "ENTER"]
+]
+
+text_welcome_lines = [
+    ["Вітаємо у грі:"],
+    ["...Хто хоче стати мільйонером?..."],
+    ["Дізнатися правила гри: натисніть ", "q"],
+    ["Для початку гри натисніть ", "ENTER"]
+]
+
+text_balls_lines = [
+    "1 000 000",
+    "500 000",
+    "250 000",
+    "125 000",
+    "64 000",
+    "32 000",
+    "16 000",
+    "8 000",
+    "4 000",
+    "2 000",
+    "1 000",
+    "500",
+    "300",
+    "200",
+    "100",
+]
+
+questions = [
+    {
+        "question": "Яка найбільша планета Сонячної системи?",
+        "answers": [
+            {"text": "A. Земля", "result": False},
+            {"text": "B. Юпітер", "result": True},
+            {"text": "C. Марс", "result": False},
+            {"text": "D. Венера", "result": False}
+        ]
+    },
+    {
+        "question": "Яка країна має найбільше населення у світі?",
+        "answers": [
+            {"text": "A. Індія", "result": False},
+            {"text": "B. США", "result": False},
+            {"text": "C. Китай", "result": True},
+            {"text": "D. Бразилія", "result": False}
+        ]
+    },
+    {
+        "question": "Скільки континентів на Землі?",
+        "answers": [
+            {"text": "A. П’ять", "result": False},
+            {"text": "B. Сім", "result": True},
+            {"text": "C. Шість", "result": False},
+            {"text": "D. Вісім", "result": False}
+        ]
+    },
+    {
+        "question": "Яка хімічна формула води?",
+        "answers": [
+            {"text": "A. H2O", "result": True},
+            {"text": "B. CO2", "result": False},
+            {"text": "C. O2", "result": False},
+            {"text": "D. NaCl", "result": False}
+        ]
+    },
+    {
+        "question": "Хто написав \"Гамлет\"?",
+        "answers": [
+            {"text": "A. Вільям Шекспір", "result": True},
+            {"text": "B. Леся Українка", "result": False},
+            {"text": "C. Тарас Шевченко", "result": False},
+            {"text": "D. Оноре де Бальзак", "result": False}
+        ]
+    },
+    {
+        "question": "Скільки хвилин у одній годині?",
+        "answers": [
+            {"text": "A. 50", "result": False},
+            {"text": "B. 60", "result": True},
+            {"text": "C. 70", "result": False},
+            {"text": "D. 100", "result": False}
+        ]
+    },
+    {
+        "question": "Який океан є найбільшим у світі?",
+        "answers": [
+            {"text": "A. Атлантичний", "result": False},
+            {"text": "B. Тихий", "result": True},
+            {"text": "C. Індійський", "result": False},
+            {"text": "D. Північний Льодовитий", "result": False}
+        ]
+    },
+    {
+        "question": "Яке найбільше місто України за населенням?",
+        "answers": [
+            {"text": "A. Одеса", "result": False},
+            {"text": "B. Дніпро", "result": False},
+            {"text": "C. Київ", "result": True},
+            {"text": "D. Львів", "result": False}
+        ]
+    },
+    {
+        "question": "Який елемент має символ \"O\" у періодичній таблиці?",
+        "answers": [
+            {"text": "A. Кисень", "result": True},
+            {"text": "B. Водень", "result": False},
+            {"text": "C. Вуглець", "result": False},
+            {"text": "D. Азот", "result": False}
+        ]
+    },
+    {
+        "question": "Скільки хребців у шиї людини?",
+        "answers": [
+            {"text": "A. 5", "result": False},
+            {"text": "B. 7", "result": True},
+            {"text": "C. 9", "result": False},
+            {"text": "D. 12", "result": False}
+        ]
+    },
+    {
+        "question": "Яка найвища гора у світі?",
+        "answers": [
+            {"text": "A. Монблан", "result": False},
+            {"text": "B. Кіліманджаро", "result": False},
+            {"text": "C. Еверест", "result": True},
+            {"text": "D. Аннапурна", "result": False}
+        ]
+    },
+    {
+        "question": "Скільки буде 7 * 8?",
+        "answers": [
+            {"text": "A. 54", "result": False},
+            {"text": "B. 56", "result": True},
+            {"text": "C. 64", "result": False},
+            {"text": "D. 48", "result": False}
+        ]
+    },
+    {
+        "question": "Який метал використовують для виробництва електропроводки?",
+        "answers": [
+            {"text": "A. Алюміній", "result": False},
+            {"text": "B. Мідь", "result": True},
+            {"text": "C. Залізо", "result": False},
+            {"text": "D. Свинець", "result": False}
+        ]
+    }
+]
+
